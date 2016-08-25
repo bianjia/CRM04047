e17ae59c40967e6419af90b16bf37873a9ab12bc
bianjia <bj_1110@163.com>
2016-08-25 16:53:47 +0800

    update022
    update022

--- src/com/atguigu/git/Git.java
+++ src/com/atguigu/git/Git.java
@@ -5,6 +5,7 @@ public class Git {
 	public static void main(String[] args) {
       System.out.println("hello 0407");
       System.out.println("hello 88887");
+      System.out.println("hello 你好");
 	}
 
 }
